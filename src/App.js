512d7e188b40b79d594b11fad04d339180bdb44d
Kate Galich <kate.galich@outlook.com>
2024-03-20 14:50:50 -0400

    adding running text line

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 
 import './App.css';
+import { ReactTyped } from "react-typed";
 
 function App() {
   return (
@@ -8,64 +9,60 @@ function App() {
       <header className="App-header">
         <img src="KatePro.JPG" className='photo'></img>
 
+        <div>
+          <code>
+          {" "}
+            <ReactTyped strings={["Welcome to my site", "I'm working on it..."]} typeSpeed={120} loop />
+          </code>
+        </div>
+
         <a href='https://www.linkedin.com/in/kate-galich/' target="_blank">
           <img src="In-Blue-96.png" className='in-logo'></img>
         </a>
       </header>
       <main className='App-main'>
-{/* 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
         <div className="glowing">
-          <span style={{"--i":1}}></span>
-          <span style={{"--i":2}}></span>
-          <span style={{"--i":3}}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
         </div>
         <div className="glowing">
-          <span style={{"--i":1}}></span>
-          <span style={{"--i":2}}></span>
-          <span style={{"--i":3}}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
         </div>
         <div className="glowing">
-          <span style={{"--i":1}}></span>
-          <span style={{"--i":2}}></span>
-          <span style={{"--i":3}}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
         </div>
         <div className="glowing">
-          <span style={{"--i":1}}></span>
-          <span style={{"--i":2}}></span>
-          <span style={{"--i":3}}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
         </div>
         <div className="glowing">
-          <span style={{"--i":1}}></span>
-          <span style={{"--i":2}}></span>
-          <span style={{"--i":3}}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
         </div>
         <div className="glowing">
-          <span style={{"--i":1}}></span>
-          <span style={{"--i":2}}></span>
-          <span style={{"--i":3}}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
         </div>
         <div className="glowing">
-          <span style={{"--i":1}}></span>
-          <span style={{"--i":2}}></span>
-          <span style={{"--i":3}}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
         </div>
         <div className="glowing">
-          <span style={{"--i":1}}></span>
-          <span style={{"--i":2}}></span>
-          <span style={{"--i":3}}></span>
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
         </div>
+
       </main>
 
       <footer>
